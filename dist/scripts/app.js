cf1abd90992105e0b4b502974da8a1ff7869e628
Tobi Weinstock <tv.weinstock@gmail.com>
2015-05-12 16:02:32 +0200

    svg graph created with hard coded temperatures

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,11 +1,25 @@
 "use strict";
 function draw(data) {
-  data = _.sortBy(data, 'temp');
-  var highestTemp = _.max(data, 'temp').temp;
-  d3.select(".container").append("div").attr("class", "bargraph").selectAll(".bar").data(data).enter().append("div").attr("class", "bar").style("width", (function(d) {
-    return d.temp / highestTemp * 100 + '%';
-  })).text((function(d) {
-    return (d.city + " (" + d.temp + ")");
+  var width = 600,
+      barHeight = 40;
+  var max = d3.max(data, (function(d) {
+    return d.temp;
+  }));
+  var xScale = d3.scale.linear().domain([0, max]).range([100, width]);
+  var chart = d3.select('.main').attr('width', width).attr('height', barHeight * data.length);
+  var g = chart.selectAll('g').data(data).enter().append('g').attr('transform', (function(d, i) {
+    return "translate(0," + i * barHeight + ")";
+  }));
+  g.append("rect").attr('width', (function(d) {
+    return xScale(d.temp);
+  })).attr('height', barHeight - 1);
+  g.append('text').attr('x', (function(d) {
+    return xScale(d.temp) - 30;
+  })).attr('y', barHeight / 2).text((function(d) {
+    return d.temp;
+  }));
+  g.append('text').attr('x', 5).attr('y', barHeight / 2).text((function(d) {
+    return d.city;
   }));
 }
 var data = [{
--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,39 @@
 function draw(data){
-  data = _.sortBy(data, 'temp');
-  var highestTemp = _.max(data, 'temp').temp; 
+    var width = 600,
+    barHeight = 40;
 
-  d3.select(".container")
-    .append("div")
-      .attr("class", "bargraph")
-    .selectAll(".bar")
-    .data(data)
-    .enter()
-      .append("div")
-        .attr("class", "bar")
-        .style("width", (d)=>
-          d.temp/highestTemp * 100 + '%')
-        .text((d) =>
-          `${d.city} (${d.temp})`)
+    var max = d3.max(data, (d)=> d.temp);
+    var xScale = d3.scale.linear()
+    .domain([0, max])
+    .range([100, width]);
+
+    var chart = d3.select('.main')
+        .attr('width', width)
+        .attr('height', barHeight * data.length);
+
+    var g = chart.selectAll('g')
+        .data(data)
+        .enter()
+        .append('g')
+        .attr('transform', (d, i)=> "translate(0," + i * barHeight + ")");
+
+    g.append("rect")
+        .attr('width', (d)=> xScale(d.temp))
+        .attr('height', barHeight - 1);
+
+    g.append('text')
+        .attr('x', (d)=> xScale(d.temp) - 30)
+        .attr('y', barHeight/2)
+        .text((d)=> d.temp);
+
+    g.append('text')
+        .attr('x', 5)
+        .attr('y', barHeight/2)
+        .text((d)=> d.city);
 
 }
 
-// var weather = [{city:"Toronto", temp:20}, {city:"Ottawa", temp:15}, {city:"San Mateo", temp:20}, {city:"Paris", temp:25}];
-var data = [{"city":"Toronto", "temp":20}, {"city":"Ottawa", "temp":15}, {"city":"San Mateo", "temp":20}, {"city":"Paris", "temp":25}];
 
+var data = [{"city":"Toronto", "temp":20}, {"city":"Ottawa", "temp":15}, {"city":"San Mateo", "temp":20}, {"city":"Paris", "temp":25}];
 draw(data);
 
