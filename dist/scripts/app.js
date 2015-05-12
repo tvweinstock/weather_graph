7446f16133cd0afb45d43a189b72e23bd7cc887f
Tobi Weinstock <tv.weinstock@gmail.com>
2015-05-12 14:12:50 +0200

    hard coded svg graph

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,4 +1,24 @@
 "use strict";
-var func = (function(a, b, c) {
-  return a + b + c;
-});
+function draw(data) {
+  data = _.sortBy(data, 'temp');
+  var highestTemp = _.max(data, 'temp').temp;
+  d3.select(".container").append("div").attr("class", "bargraph").selectAll(".bar").data(data).enter().append("div").attr("class", "bar").style("width", (function(d) {
+    return d.temp / highestTemp * 100 + '%';
+  })).text((function(d) {
+    return (d.city + " (" + d.temp + ")");
+  }));
+}
+var data = [{
+  "city": "Toronto",
+  "temp": 20
+}, {
+  "city": "Ottawa",
+  "temp": 15
+}, {
+  "city": "San Mateo",
+  "temp": 20
+}, {
+  "city": "Paris",
+  "temp": 25
+}];
+draw(data);
--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,24 @@
-var func = (a,b,c) => a+b+c;
+function draw(data){
+  data = _.sortBy(data, 'temp');
+  var highestTemp = _.max(data, 'temp').temp; 
+
+  d3.select(".container")
+    .append("div")
+      .attr("class", "bargraph")
+    .selectAll(".bar")
+    .data(data)
+    .enter()
+      .append("div")
+        .attr("class", "bar")
+        .style("width", (d)=>
+          d.temp/highestTemp * 100 + '%')
+        .text((d) =>
+          `${d.city} (${d.temp})`)
+
+}
+
+// var weather = [{city:"Toronto", temp:20}, {city:"Ottawa", temp:15}, {city:"San Mateo", temp:20}, {city:"Paris", temp:25}];
+var data = [{"city":"Toronto", "temp":20}, {"city":"Ottawa", "temp":15}, {"city":"San Mateo", "temp":20}, {"city":"Paris", "temp":25}];
+
+draw(data);
+
