3b0f3cfb352a0d333c200d66bd1cdf16a6e402c1
Tobi Weinstock <tv.weinstock@gmail.com>
2015-05-13 14:30:31 +0200

    added negative value chart

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -22,6 +22,40 @@ function draw(data) {
     return d.city;
   }));
 }
+function draw2(data2) {
+  var margin = {
+    top: 30,
+    right: 10,
+    bottom: 10,
+    left: 30
+  },
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom,
+      barPadding = 1;
+  var y0 = Math.max(Math.abs(d3.min(data2)), Math.abs(d3.max(data2)));
+  var y = d3.scale.linear().domain([-y0, y0]).range([height, 0]).nice();
+  var x = d3.scale.ordinal().domain(d3.range(data2.length)).rangeRoundBands([0, width], .2);
+  var yAxis = d3.svg.axis().scale(y).orient("left");
+  var svg = d3.select(".container2").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  svg.selectAll(".bar").data(data2).enter().append("rect").attr("class", function(d) {
+    return d < 0 ? "bar negative" : "bar positive";
+  }).attr("y", function(d) {
+    return y(Math.max(0, d));
+  }).attr("x", function(d, i) {
+    return x(i);
+  }).attr("height", function(d) {
+    return Math.abs(y(d) - y(0));
+  }).attr("width", x.rangeBand());
+  var g = svg.append("g").attr("class", "x axis").call(yAxis);
+  svg.append("g").attr("class", "y axis").append("line").attr("y1", y(0)).attr("y2", y(0)).attr("x1", 0).attr("x2", width);
+  g.selectAll("text").data(data2).enter().append("text").text(function(d) {
+    return d;
+  }).attr("text-anchor", "middle").attr("x", function(d, i) {
+    return i * (width / data2.length) + (width / data2.length - barPadding) / 2;
+  }).attr("y", function(d) {
+    return height - (d * 4) + 14;
+  }).attr("font-family", "sans-serif").attr("font-size", "11px").attr("fill", "white");
+}
 var cityWeather = 'http://api.openweathermap.org/data/2.5/group?id=6094817,5392423,6167865,2988507&units=metric';
 $.get(cityWeather, function(weather) {
   var response = weather;
@@ -42,5 +76,7 @@ $.get(cityWeather, function(weather) {
     "city": "Paris",
     "temp": parisTemp
   }];
+  var data2 = [torontoTemp, ottawaTemp, sanMateoTemp, parisTemp];
   draw(data);
+  draw2(data2);
 });
